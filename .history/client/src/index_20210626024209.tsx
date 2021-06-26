52e2f733b413e2d90723fb0394dc67baff83a171
joaolopes <byjoao.lopes@bycom.pt>
2021-06-26 02:44:38 +0100

    Analytics

--- .history/client/src/index_20210626024209.tsx
+++ .history/client/src/index_20210626024209.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import ReactGA from 'react-ga'
 
 import App from './App'
 import { reducer } from './state/reducer'
 import { StateProvider } from './state/state'
-
-ReactGA.initialize('UA-200630534-1')
-ReactGA.pageview(window.location.pathname + window.location.search)
 
 ReactDOM.render(
 	<React.StrictMode>
