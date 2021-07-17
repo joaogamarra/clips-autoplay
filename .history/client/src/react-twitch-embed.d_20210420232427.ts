1728b4eb81935ffcf9e9c5f50d9c949913e435fd
Serguei <serkhas@hotmail.com>
2021-07-17 13:24:36 -0400

    remove gitignored files

--- .history/client/src/react-twitch-embed.d_20210420232427.ts
+++
@@ -1,1 +0,0 @@
-declare module 'react-twitch-embed'
--- .history/server/src/database/models/twitch_20210423201103.ts
+++
@@ -1,8 +0,0 @@
-import Mongoose from 'mongoose'
-
-const twitchSearchSchema = new Mongoose.Schema({
-	id: Number,
-	login: String,
-})
-
-export const TwitchSearch = Mongoose.model('twitchSearch', twitchSearchSchema)
